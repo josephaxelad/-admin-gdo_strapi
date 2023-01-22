1035e6a05e3e270ee9901973228b533ea6a1f370
Joseph Axel AKE-DANHO <josephaxelad@gmail.com>
2023-01-22 16:54:13 +0000

    22.01.2023 commit

---
+++ config/middlewares.js
@@ -0,0 +1,38 @@
+module.exports = [
+  'strapi::errors',
+  // 'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'generationdoree.s3.eu-west-3.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'generationdoree.s3.eu-west-3.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
